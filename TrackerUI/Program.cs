1e9bf1fbcfdbffb1a15c7d23e871c26bd96bec0a
jccchen103 <jac186@ucsd.edu>
2019-12-04 02:37:50 -0800

    added TrackerLibrary to the TrackerUI project as a reference

--- TrackerUI/Program.cs
+++ TrackerUI/Program.cs
@@ -16,6 +16,10 @@ namespace TrackerUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            // Initialize data connections
+            TrackerLibrary.GlobalConfig.InitializeConnections(true, true);
+
             Application.Run(new TournamentDashboardForm());
         }
     }
