a1b4c1c8d5344e91b7c1f7dd09ccd1fb823936c1
jccchen103 <jac186@ucsd.edu>
2019-12-10 18:57:38 -0800

    connected to mySQL database and able to save prizes to the sql db

--- TrackerLibrary/DataAccess/SqlConnector.cs
+++ TrackerLibrary/DataAccess/SqlConnector.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
 using MySql.Data.MySqlClient;
 using TrackerLibrary.Models;
 
@@ -20,8 +22,21 @@ namespace TrackerLibrary.DataAccess
         /// <returns>The prize information that was inserted, including the id.</returns>
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            // TODO: Save to prizes table in db.
-            // TODO: Get id of the inserted prize and set Id of model.
+            using (IDbConnection connection = new MySqlConnection(GlobalConfig.GetConnectionString("Tournaments")))
+            {
+                // TODO: Save to prizes table in db.
+                var p = new DynamicParameters();
+                p.Add("p_place_number", model.PlaceNumber);
+                p.Add("p_place_name", model.PlaceName);
+                p.Add("p_prize_amount", model.PrizeAmount);
+                p.Add("p_percentage", model.PrizePercentage);
+                p.Add("@p_id", dbType: DbType.Int32, direction: ParameterDirection.Output);
+
+                connection.Execute("prizes_insert", p, commandType: CommandType.StoredProcedure);
+
+                // TODO: Get id of the inserted prize and set Id of model.
+                model.Id = p.Get<int>("@p_id");
+            }
             return model;
         }
     }
